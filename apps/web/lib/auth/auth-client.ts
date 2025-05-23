0fd72e409679af3a099d32414159d934edc1084a
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-05-23 23:59:36 +0200

    fix: Properly handle API URL

--- apps/web/lib/auth/auth-client.ts
+++ apps/web/lib/auth/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
+const host = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
 export const authClient = createAuthClient({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/auth/api/auth`,
+  baseURL: `${host}/auth/api/auth`,
   plugins: [inferAdditionalFields({
     user: {
       first_name: {
