b98397d18f8fab0ca6831bc5ff5fb105694ef44e
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-10-09 19:35:20 +0200

    feat: Enhance info display with dynamic messages

--- apps/web/components/progress/total-hours-info.tsx
+++ apps/web/components/progress/total-hours-info.tsx
@@ -1,13 +1,25 @@
 import { Alert, AlertDescription, AlertTitle } from "@repo/ui/components/alert";
 import { Info } from "lucide-react";
 
+function hoursInfoText(hours: number) {
+  switch (hours) {
+    case 0:
+      return "You haven't been to the dance school yet.";
+    case 1:
+      return "You have spent 1 hour learning in our dance school.";
+    default:
+      return `You have spent ${hours} hours in our dance school.`;
+  }
+}
+
 export function TotalHoursInfo({ hours }: { hours: number; }) {
+
   return (
     <Alert variant="default">
       <Info />
       <AlertTitle>Fun fact!</AlertTitle>
       <AlertDescription>
-        You have spent {hours} hours in our dance school.
+        {hoursInfoText(hours)}
       </AlertDescription>
     </Alert>
   );
