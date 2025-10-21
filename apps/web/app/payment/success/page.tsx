6d672e29760ad578194d50371f8adce18cdb56e2
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-10-21 20:02:38 +0200

    feat: Add payment success page

---
+++ apps/web/app/payment/success/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import { CircleCheckBig } from "lucide-react";
+
+export default function Page() {
+  return (
+    <div className="h-full p-4 flex items-center justify-center">
+      <div className="w-full max-w-lg flex flex-col text-center items-center">
+        <CircleCheckBig className="size-48 text-green-500 mb-16" />
+        <h1 className="text-3xl md:text-4xl font-bold">Payment Successful</h1>
+        <p className="text-lg md:text-xl text-muted-foreground">Your payment has been successful. You can now access your tickets in the <Link href="/user/tickets" className="text-blue-500 hover:text-blue-600">My Tickets</Link> page.</p>
+      </div>
+    </div>
+  );
+}
