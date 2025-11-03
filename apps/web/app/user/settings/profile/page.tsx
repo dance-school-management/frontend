a92461f72139379a29553894ddc7a051218c9870
Mateusz Wala <mateusz.wala2734@gmail.com>
2025-11-03 17:52:03 +0100

    feat: Add settings section

---
+++ apps/web/app/user/settings/profile/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/ui/card";
+
+export default function ProfilePage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Profile</CardTitle>
+        <CardDescription>This section is coming soon.</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
+
